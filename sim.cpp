85cb63870abd71eecdd1d074de1683ea4b6fd8f6
JDARIMAN <manzanillad.jandarius@gmail.com>
2026-09-02 15:30:00 -0400

    Quick save before refactoring declaration order

--- sim.cpp
+++ sim.cpp
@@ -20,7 +20,7 @@ Parametric Monte Carlo Simulation
 // Note
 /* 
 Any funny comments that start with !, ?, * are because I use the Better Comments VSCODE extention -these comments are mainly for me.
-This project is aimed for me to learn and dive deep into the understanding of particle filters and localization alorhithims. 
+This project is aimed for me to learn and dive deep into the understanding of ParticleType filters and localization alorhithims. 
 This project will constantly be subject to change as I learn more and find more configurations for me to set up. This code will be riddled with comments
 */
 
@@ -30,7 +30,7 @@ This project will constantly be subject to change as I learn more and find more 
 // ! STATE SPACE DIMENSIONS (ROBOT MOVEMENT DIMENSIONS)
 enum class DriveConfig {
     Tank, //* Moves in 2 dimensions, as it can't move latterally: X(locked), Y, Yaw
-    OmniDirectional, //* Moves in 3 dimensions, as the x dimension is now free: X, Y, Yaw
+    Omni, //* Moves in 3 dimensions, as the x dimension is now free: X, Y, Yaw
     Drone, //* Adheres to the usual dimensionality of objects in a 3D space: X, Y, Z, Pitch, Roll, Yaw
 };
 
@@ -41,35 +41,126 @@ enum class LocalizationType {
 
 // ! THETA REPRESENTS ROBOT HEADING
 // learning templates, they're pretty cool
-template <DriveConfig Drive> struct Particle; // ? Must declare templates before assigning them;
+// particle structs
+template <DriveConfig Drive> struct ParticleType; // ? Must declare templates before assigning them;
 
-template <> struct Particle<DriveConfig::Tank> {
+template <> struct ParticleType<DriveConfig::Tank> {
     double x = 0;
     double y = 0;
     double yaw = 0;
-    double weight = 0;
+    double weight = 1;
 };
 
-template <> struct Particle<DriveConfig::OmniDirectional> {
+template <> struct ParticleType<DriveConfig::Omni> {
     double x = 0;
     double y = 0;
     double yaw = 0;
-    double weight = 0;
+    double weight = 1;
 };
 
-template <> struct Particle<DriveConfig::Drone> {
+template <> struct ParticleType<DriveConfig::Drone> {
     double x = 0;
     double y = 0;
     double z = 0;
     double pitch = 0;
     double roll = 0;
     double yaw = 0;
-    double weight = 0;
+    double weight = 1;
+};
+
+//* robot parameter structs
+template <DriveConfig Drive> struct PhysParams;
+
+template <> struct PhysParams<DriveConfig::Tank> {
+    double track_width;
+};
+
+template <> struct PhysParams<DriveConfig::Omni> {
+    int num_wheels;
+    double wheel_mnt_ang; // wheel mounting angle
+    double radius; // radius from robot center in meters
+};
+
+template <> struct PhysParams<DriveConfig::Drone> {
+
+};
+
+// motion structs
+template <DriveConfig Drive> struct Velocity;
+
+template <> struct Velocity<DriveConfig::Tank> {double vl, vr, theta = 0;}; // velocity left and right
+
+template <> struct Velocity<DriveConfig::Omni> {double v1, v2, v3, v4, theta = 0;}; // 
+
+template <> struct Velocity<DriveConfig::Drone> {double vx, vy, vz, qw, qx, qy, qz;}; // ! LEARN QUATERNIAN ANGLES
+
+
+// sensor configs
+enum class SensorType {ToFSensor, LandmarkBearing, LiDAR};
+
+template <SensorType Sensor> struct Measurement;
+
+template <> struct Measurement<SensorType::ToFSensor> { // your ultrasonic and lazer distance sensors
+    double dist;
+};
+
+template <> struct Measurement<SensorType::LandmarkBearing> { // specialized cameras
+    double dist, angle;
+    int landmark_id;
+};
+
+template <> struct Measurement<SensorType::LiDAR> {
+    std::vector<double> ranges; // allots all our measurements in 1 place since lidar takes a bunch
+};
+
+// Robot base Template
+template <DriveConfig Drive> class Robot { // ? template determines what type of robot it is, no need to declare it as an instance variable
+    private:
+    int num_particles;
+    ParticleType<Drive> particles;
+    public:
+    // constructor
+
+    // functions
+    void predcit(std::chrono::seconds dt, Velocity ctrl_input) {
+        if constexpr (Drive == DriveConfig::Tank){
+            
+        }
+        else if constexpr (Drive == DriveConfig::Omni){
+
+        }
+        else if constexpr (Drive == DriveConfig::Drone){
+
+        }
+        else { // just incase something happen
+
+        }
+    }
+
 };
 
 
 
 
+
+// Map point templates
+template <DriveConfig Drive> struct Point;
+template <> struct Point<DriveConfig::Tank> {double x, y;};
+template <> struct Point<DriveConfig::Omni> {double x,y;};
+template <> struct Point <DriveConfig::Drone> {double x, y, z;};
+
+
+
+template <LocalizationType lcl> class Map {
+    
+};
+
+template <DriveConfig Drive, SensorType Sensor>class MCLSim {
+    private:
+    Robot<Drive> bot;
+    public:
+
+};
 
 
 
