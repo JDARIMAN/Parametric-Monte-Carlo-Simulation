27647698376f29c1405a57128d457ed1ee2f8d9d
JDARIMAN <manzanillad.jandarius@gmail.com>
2026-09-12 17:52:15 -0400

    "Finished" Prediction Step for Tank Drive

--- sim.cpp
+++ sim.cpp
@@ -16,6 +16,7 @@ Parametric Monte Carlo Simulation
 // Paralel Processing Libraries I've worked with
 #include <atomic>
 #include <thread>
+// ! IMPLEMENT SOMETHING FOR FRONT END
 
 // Note
 /* 
@@ -23,6 +24,7 @@ Any funny comments that start with !, ?, * are because I use the Better Comments
 This project is aimed for me to learn and dive deep into the understanding of ParticleType filters and localization alorhithims. 
 This project will constantly be subject to change as I learn more and find more configurations for me to set up. This code will be riddled with comments
 */
+
 
 
 /*Data Structures*/
@@ -68,47 +70,68 @@ template <> struct ParticleType<DriveConfig::Drone> {
     double weight = 1;
 };
 
-//* robot parameter structs
+// sensor configs
 template <DriveConfig Drive> struct PhysParams;
-
 template <> struct PhysParams<DriveConfig::Tank> {
     double track_width;
 };
-
 template <> struct PhysParams<DriveConfig::Omni> {
     int num_wheels;
     double wheel_mnt_ang; // wheel mounting angle
     double radius; // radius from robot center in meters
 };
-
 template <> struct PhysParams<DriveConfig::Drone> {
 
 };
 
 // motion structs
-template <DriveConfig Drive> struct Velocity;
+template <DriveConfig Drive> struct Velocity; // ? m/s and rad/s
+template <> struct Velocity<DriveConfig::Tank> {double vl, vr, w = 0;}; // velocity left and right, and angular; w represents angular velocity
+template <> struct Velocity<DriveConfig::Omni> {double v1, v2, v3, v4, w = 0, vx, vy;}; // ! figure out the inverse kinematics
+template <> struct Velocity<DriveConfig::Drone> {double vx, vy, vz, wx = 0, wy = 0, wz = 0;}; // includes angular velocity for all directions
 
-template <> struct Velocity<DriveConfig::Tank> {double vl, vr, theta = 0;}; // velocity left and right
+template <DriveConfig Drive> struct RoboPose;
+template <> struct RoboPose<DriveConfig::Tank> {double x, y, theta = 0;};
+template <> struct RoboPose<DriveConfig::Omni> {double x, y, theta = 0;};
+template <> struct RoboPose<DriveConfig::Drone> {double x, y, z, qw, qx, qy, qz;}; // ! LEARN QUATERNIAN ANGLES
 
-template <> struct Velocity<DriveConfig::Omni> {double v1, v2, v3, v4, theta = 0;}; // 
+// noise parameter struct; default noise values given by claude
+template <DriveConfig Drive> struct NoiseParams;
 
-template <> struct Velocity<DriveConfig::Drone> {double vx, vy, vz, qw, qx, qy, qz;}; // ! LEARN QUATERNIAN ANGLES
+template <> struct NoiseParams<DriveConfig::Tank> {
+    double a1 = 0.1; // rot1 error from rotation
+    double a2 = 0.1; // rot1 error from translation
+    double a3 = 0.1; // trans error from translation
+    double a4 = 0.1; // trans error from rotation
+};
+
+template <> struct NoiseParams<DriveConfig::Omni> {
+    double a1 = 0.1; // v_x noise scaling
+    double a2 = 0.1; // cross-term v_y->v_x noise
+    double a3 = 0.1; // omega noise scaling
+    double a4 = 0.1; // cross-term v->omega noise
+};
+
+template <> struct NoiseParams<DriveConfig::Drone> {
+    double vxn = 0.05;   // linear velocity noise stddev source (m/s)
+    double vyn = 0.05;   // vertical velocity noise stddev source (m/s)
+    double rolln = 0.02;   // roll rate noise stddev source (rad/s)
+    double pitchn = 0.02; // pitch rate noise stddev source (rad/s)
+    double yawn = 0.02;   // yaw rate noise stddev source (rad/s)
+};
 
 
 // sensor configs
 enum class SensorType {ToFSensor, LandmarkBearing, LiDAR};
 
 template <SensorType Sensor> struct Measurement;
-
 template <> struct Measurement<SensorType::ToFSensor> { // your ultrasonic and lazer distance sensors
     double dist;
 };
-
 template <> struct Measurement<SensorType::LandmarkBearing> { // specialized cameras
     double dist, angle;
     int landmark_id;
 };
-
 template <> struct Measurement<SensorType::LiDAR> {
     std::vector<double> ranges; // allots all our measurements in 1 place since lidar takes a bunch
 };
@@ -116,14 +139,44 @@ template <> struct Measurement<SensorType::LiDAR> {
 // Robot base Template
 template <DriveConfig Drive> class Robot { // ? template determines what type of robot it is, no need to declare it as an instance variable
     private:
-    int num_particles;
-    ParticleType<Drive> particles;
+    RoboPose<Drive> pose;
+    PhysParams<Drive> bot;
+    NoiseParams<Drive> noise;
+    std::mt19937 rng_engine(seed); // random bit generator from the random lib
     public:
     // constructor
 
     // functions
-    void predcit(std::chrono::seconds dt, Velocity ctrl_input) {
+    double sampleNoise(double b) { // sample noise model given by claude
+        double sigma = std::sqrt(b);
+        std::normal_distribution<double> dist(0.0, sigma);
+        return dist(rng_engine);
+    }
+    
+    void predict(std::chrono::seconds dt, Velocity<Drive>ctrl_input) {
         if constexpr (Drive == DriveConfig::Tank){
+            double v = (ctrl_input.vr + ctrl_input.vl)/2;
+            ctrl_input.w = (ctrl_input.vr - ctrl_input.vl)/bot.track_width; 
+            // update pose based on velocities
+            // ? n_ denotes new
+            double n_x = pose.x + v*std::cos(pose.theta)*dt;
+            double n_y = pose.y + v*std::cos(pose.theta)*dt;
+            double n_theta = pose.theta + w*dt;
+
+            // decompose motion
+            double dr1 = std::atan2(n_y-pose.y, n_x-pose.x) - pose.theta; // delta rotation
+            double dtrans = std::sqrt((n_x-pose.x)*(n_x-pose.x) + (n_y-pose.y)*(n_y-pose.y)); // delta translation
+            double dr2 = n_theta - pose.theta - dr1;
+
+            // sample noisy version
+            double dr1_est = dr1 - sampleNoise(noise.a1*std::abs(dr1) + noise.a2*dtrans);
+            double dtrans_est = dtrans - sampleNoise(noise.a3*dtrans + noise.a4*(std::abs(dr1) + std::abs(dr2)));
+            double dr2_est = dr2 - sampleNoise(a1*std::abs(dr2) + a2*dtrans);
+
+            // set new to old and apply noise
+            pose.x = n_x + dtrans_est*std::cos(n_theta + dr1_est);
+            pose.y = n_y + dtrans_est*std::sin(n_theta + dr1_est);
+            pose.theta = n_theta + dr1_est + dr2_est;
             
         }
         else if constexpr (Drive == DriveConfig::Omni){
@@ -143,21 +196,18 @@ template <DriveConfig Drive> class Robot { // ? template determines what type of
 
 
 
-// Map point templates
-template <DriveConfig Drive> struct Point;
-template <> struct Point<DriveConfig::Tank> {double x, y;};
-template <> struct Point<DriveConfig::Omni> {double x,y;};
-template <> struct Point <DriveConfig::Drone> {double x, y, z;};
-
-
 
 template <LocalizationType lcl> class Map {
+    private:
     
 };
 
 template <DriveConfig Drive, SensorType Sensor>class MCLSim {
     private:
     Robot<Drive> bot;
+    // particle related instance variables
+    int num_particles;
+    ParticleType<Drive> particles;
     public:
 
 };
